87dfd9290429e7a01b82ed9f532cf267f22cdbc9
george <george@deepbrain.ai>
2018-07-30 15:54:51 +0800

    增加获取mp3时长功能

--- thirdboot/src/main/java/com/deep/MediaPlayTrain.java
+++ thirdboot/src/main/java/com/deep/MediaPlayTrain.java
@@ -3,10 +3,16 @@ package com.deep;
 import it.sauronsoftware.jave.Encoder;
 import it.sauronsoftware.jave.EncoderException;
 import it.sauronsoftware.jave.MultimediaInfo;
+import javazoom.jl.decoder.Bitstream;
+import javazoom.jl.decoder.BitstreamException;
+import javazoom.jl.decoder.Header;
 
 import java.io.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.channels.FileChannel;
 
 /**
@@ -17,8 +23,9 @@ import java.nio.channels.FileChannel;
 public class MediaPlayTrain {
     public static void main(String[] args) {
         File soundMedia = new File("C:\\Users\\Administrator\\Downloads\\SLA.avi");
-        getMediaSize(soundMedia);
-        getMediaTime(soundMedia);
+//        getMediaSize(soundMedia);
+//        getMediaTime(soundMedia);
+        getMp3Time("http://cdnmusic.hezi.360iii.net/textbook/0201/02010201/242455838124.mp3");
     }
 
     /**
@@ -27,7 +34,7 @@ public class MediaPlayTrain {
      * @param file
      * @return
      */
-    private static String getMediaSize(File file) {
+    public static String getMediaSize(File file) {
         FileChannel fc = null;
         String size = "";
         try {
@@ -58,7 +65,7 @@ public class MediaPlayTrain {
      * @param file
      * @return
      */
-    private static String getMediaTime(File file) {
+    public static String getMediaTime(File file) {
         Encoder encoder = new Encoder();
         String length = "";
         try {
@@ -73,5 +80,40 @@ public class MediaPlayTrain {
         }
         System.out.println(length);
         return length;
+    }
+
+    /**
+     * 功能描述:获取网络音乐文件时长
+     *
+     * @param url 音频文件url地址
+     */
+    public static String getMp3Time(String url) {
+        String time = "";
+        try {
+            URL urlFile = new URL(url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) urlFile.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            int code = 0;
+            code = httpURLConnection.getResponseCode();
+            int length = httpURLConnection.getContentLength();
+            if (code == 200) {
+                BufferedInputStream bis = new BufferedInputStream(httpURLConnection.getInputStream());
+                Bitstream bt = new Bitstream(bis);
+                Header h = bt.readFrame();
+                long times = (long) (h.total_ms(length) / 1000);//音频秒数
+                int hour = (int) times / 3600;
+                int minute = (int) (times % 3600) / 60;
+                int seconds = (int) (times - hour * 3600 - minute * 60);
+                time = hour + ":" + minute + ":" + seconds;
+                System.out.println(time);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BitstreamException e) {
+            e.printStackTrace();
+        }
+        return time;
     }
 }
