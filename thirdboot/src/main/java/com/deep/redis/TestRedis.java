5a9e62de6ace905d0df2ccbeabca1517a1c2063b
george <george@deepbrain.ai>
2018-08-22 17:27:07 +0800

    redis的事务

--- thirdboot/src/main/java/com/deep/redis/TestRedis.java
+++ thirdboot/src/main/java/com/deep/redis/TestRedis.java
@@ -1,6 +1,7 @@
 package com.deep.redis;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 
 /**
  * @Author: george
@@ -10,7 +11,16 @@ import redis.clients.jedis.Jedis;
 public class TestRedis {
     public static void main(String[] args){
         Jedis jedis = new Jedis("192.168.81.129");
+        jedis.incr("list");
+        jedis.watch("list");
         String str = jedis.get("list");
         System.out.println(str);
+    }
+
+    private void trasaction(Jedis jedis){
+        Transaction transaction = jedis.multi();
+        jedis.incr("list");
+        jedis.incr("test");
+        transaction.exec();
     }
 }
