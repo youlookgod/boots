3cfb5ecc8155efd65b2a51bf98dce91ce6e44dc8
george <george@deepbrain.ai>
2018-09-14 15:16:56 +0800

    结构修改

--- thirdboot/src/main/java/com/training/CollectionTrain/TestMuliptMap.java
+++ thirdboot/src/main/java/com/training/CollectionTrain/TestMuliptMap.java
@@ -15,7 +15,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class TestMuliptMap {
     public static void main(String[] args) {
-
+        testMultiMap();
     }
 
     private static void testMultiMap() {
